8ecd895c2630932b93f7b0daeb479eb6aa85f500
zhang.bo <2421715443@qq.com>
2021-09-03 10:17:48 +0800

    修改文件

--- src/main/java/com/zanbo/leetcode/corporate_flight_bookings/Corporate_flight_bookings.java
+++ src/main/java/com/zanbo/leetcode/corporate_flight_bookings/Corporate_flight_bookings.java
@@ -2,7 +2,7 @@ package com.zanbo.leetcode.corporate_flight_bookings;
 
 /**
  * @program: Solution
- * @description:这里有 n 个航班，它们分别从 1 到 n 进行编号。
+ * @description: 这里有 n 个航班，它们分别从 1 到 n 进行编号。
  * <p>
  * 有一份航班预订表 bookings ，表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi] 意味着在从 firsti 到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。
  * <p>
@@ -21,8 +21,8 @@ public class Corporate_flight_bookings {
         int[] prefixAnd = solution.corpFlightBookings(new int[][]{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}}, 5);
         long endTime = System.currentTimeMillis();
         System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
-        for (int i = 0; i < prefixAnd.length; i++) {
-            System.out.println(prefixAnd[i]);
+        for (int i : prefixAnd) {
+            System.out.println(i);
         }
     }
 }
