562ada82068015f073fc69e7f4fee904a817dabd
zhang.bo <2421715443@qq.com>
2021-09-17 09:59:19 +0800

    状态转移

---
+++ src/main/java/com/zanbo/leetcode/Valid_sudoku.java
@@ -0,0 +1,51 @@
+package com.zanbo.leetcode;
+
+/**
+ * 请你判断一个 9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+ * <p>
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+ * 数独部分空格内已填入了数字，空白格用 '.' 表示。
+ * <p>
+ * 注意：
+ * <p>
+ * 一个有效的数独（部分已被填充）不一定是可解的。
+ * 只需要根据以上规则，验证已经填入的数字是否有效即可。
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-sudoku
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zanbo
+ * @program Solution
+ * @date 2021-09-17 上午 9:20
+ */
+public class Valid_sudoku {
+    public static void main(String[] args) {
+    
+    }
+}
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] block = new boolean[9][9];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.') {
+                    int kk = ((i / 3) * 3) + (j / 3);
+                    if (row[i][board[i][j] - '1'] || col[j][board[i][j] - '1'] || block[kk][board[i][j] - '1']) {
+                        return false;
+                    }
+                    row[i][board[i][j] - '1'] = true;
+                    col[j][board[i][j] - '1'] = true;
+                    block[kk][board[i][j] - '1'] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
